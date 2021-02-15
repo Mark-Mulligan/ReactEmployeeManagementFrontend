9bfad8c8c3ab5914b758367a3f5be473874457a4
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-02-14 19:15:43 -0600

    create Employee page code refactoring

--- src/pages/CreateEmployeePage.js
+++ src/pages/CreateEmployeePage.js
@@ -1,64 +1,66 @@
 import React from "react";
+import axios from "axios";
 import { Form, Col, Button } from "react-bootstrap";
-import axios from 'axios';
-
+import CustomFormInput from '../components/CustomFormInput';
 
 class CreateEmployeePage extends React.Component {
   state = {
-    firstName: '',
-    lastName: '',
+    firstName: "",
+    lastName: "",
     departmentId: null,
     roleId: null,
     managerId: null,
     departmentValues: [],
     roleValues: [],
-    managerValues: []
-  }
+    managerValues: [],
+  };
 
   getDepartmentValues = async () => {
-    const { data } = await axios.get("http://localhost:3001/departments/name-id");
+    const { data } = await axios.get(
+      "http://localhost:3001/departments/name-id"
+    );
     this.setState({ departmentValues: data });
   };
 
   getRolesValues = async (departmentId) => {
-    const { data } = await axios.get(`http://localhost:3001/departments/${departmentId}/roles`);
+    const { data } = await axios.get(
+      `http://localhost:3001/departments/${departmentId}/roles`
+    );
     this.setState({ roleValues: data });
-  }
+  };
 
   getManagerValues = async () => {
-    const { data } = await axios.get("http://localhost:3001/employees/name-id/exclude-id/1");
+    const { data } = await axios.get(
+      "http://localhost:3001/employees/name-id/exclude-id/1"
+    );
     this.setState({ managerValues: data });
-  }
+  };
 
   handleFormSubmit = (event) => {
     event.preventDefault();
     console.log(this.state);
-    console.log('form submitted');
-  }
+    console.log("form submitted");
+  };
 
   handleDepartmentSelect = (event) => {
     const departmentId = event.target.value;
     this.getRolesValues(departmentId);
-    this.setState({departmentId: departmentId});
-  }
+    this.setState({ departmentId: departmentId });
+  };
 
   componentDidMount() {
     this.getDepartmentValues();
     this.getManagerValues();
   }
 
-  renderDepartmentOptions() {
-    if (this.state.departmentValues.length > 0) {
-      return this.state.departmentValues.map(department => {
-        return <option key={department.id} value={department.id}>{department.name}</option>
-      })
-    }
-  }
-
   renderOptions(dataArr, key1, key2) {
     if (dataArr.length > 0) {
-      return dataArr.map(data => {
-        return <option key={data[key1]} value={data[key1]}>{data[key2]}</option>
+      return dataArr.map((data) => {
+        return (
+          <option key={data[key1]} value={data[key1]}>
+            {data[key2]}
+          </option>
+        );
       });
     }
   }
@@ -68,62 +70,86 @@ class CreateEmployeePage extends React.Component {
       <div className="container">
         <Form onSubmit={this.handleFormSubmit}>
           <Form.Row>
-            <Form.Group as={Col} controlId="formGridEmail">
-              <Form.Label>First Name</Form.Label>
-              <Form.Control 
-                type="text" 
-                placeholder="First" 
-                onChange={(event) => this.setState({firstName: event.target.value})}
-                value={this.firstName}
-                />
-            </Form.Group>
-            <Form.Group as={Col} controlId="formGridPassword">
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control 
-                type="text" 
-                placeholder="Last" 
-                onChange={(event) => this.setState({lastName: event.target.value})}
+            <Col xs={12} sm={6}>
+                <CustomFormInput
+                  controlId="firstNameInput" 
+                  label="First Name" 
+                  type="text"
+                  placeholder="First"
+                  onInputChange={(event) => this.setState({ firstName: event.target.value })}
+                  value={this.firstName}  
+                  />
+            </Col>
+            <Col xs={12} sm={6}>
+              <CustomFormInput 
+                controlId="lastNameInput"
+                label="Last Name"
+                type="text"
+                placeholder="Last"
+                onInputChange={event => this.setState({ lastName: event.target.value })}
                 value={this.lastName}
-                />
-            </Form.Group>
+              />
+            </Col>
           </Form.Row>
 
           <Form.Row>
-            <Form.Group as={Col} controlId="formDepartment">
-              <Form.Label>Department</Form.Label>
-              <Form.Control 
-                as="select" 
-                onChange={this.handleDepartmentSelect}
-                value={this.departmentId}
+            <Col sm={12} md={4}>
+              <Form.Group controlId="departmentSelect">
+                <Form.Label>Department</Form.Label>
+                <Form.Control
+                  as="select"
+                  onChange={this.handleDepartmentSelect}
+                  value={this.departmentId}
                 >
-                <option value={null}>Choose</option>
-                {this.renderOptions(this.state.departmentValues, 'id', 'name')}
-              </Form.Control>
-            </Form.Group>
+                  <option hidden>Choose</option>
+                  {this.renderOptions(
+                    this.state.departmentValues,
+                    "id",
+                    "name"
+                  )}
+                </Form.Control>
+              </Form.Group>
+            </Col>
 
-            <Form.Group as={Col} controlId="formGridState">
-              <Form.Label>Role</Form.Label>
-              <Form.Control 
-                as="select"
-                onChange={(event) => this.setState({roleId: event.target.value})}
-                value={this.roleId}
+            <Col sm={12} md={4}>
+              <Form.Group controlId="formGridState">
+                <Form.Label>Role</Form.Label>
+                <Form.Control
+                  as="select"
+                  onChange={(event) =>
+                    this.setState({ roleId: event.target.value })
+                  }
+                  value={this.roleId}
                 >
-                {this.state.departmentId ? <option value={null}>Choose...</option> : <option value={null}>Select Department</option>}
-                {this.renderOptions(this.state.roleValues, 'id', 'title')}
-              </Form.Control>
-            </Form.Group>
+                  {this.state.departmentId ? (
+                    <option hidden>Choose</option>
+                  ) : (
+                    <option hidden>Must select department</option>
+                  )}
+                  {this.renderOptions(this.state.roleValues, "id", "title")}
+                </Form.Control>
+              </Form.Group>
+            </Col>
 
-            <Form.Group as={Col} controlId="formGridZip">
-              <Form.Label>Manager</Form.Label>
-              <Form.Control 
-                as="select" 
-                onChange={(event) => this.setState({managerId: event.target.value})}
-                value={this.managerId}
+            <Col sm={12} md={4}>
+              <Form.Group controlId="formGridZip">
+                <Form.Label>Manager</Form.Label>
+                <Form.Control
+                  as="select"
+                  onChange={(event) =>
+                    this.setState({ managerId: event.target.value })
+                  }
+                  value={this.managerId}
                 >
-                <option>Choose...</option>
-                {this.renderOptions(this.state.managerValues, 'id', 'manager')}
-              </Form.Control>
-            </Form.Group>
+                  <option hidden>Choose</option>
+                  {this.renderOptions(
+                    this.state.managerValues,
+                    "id",
+                    "manager"
+                  )}
+                </Form.Control>
+              </Form.Group>
+            </Col>
           </Form.Row>
 
           <Button variant="primary" type="submit">
