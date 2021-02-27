2acc8459ee88ffb07d50aa845187a71b65b561eb
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-02-26 23:15:39 -0600

    refactored axios requests

--- src/pages/EmployeePage.js
+++ src/pages/EmployeePage.js
@@ -1,40 +1,65 @@
 import React from "react";
-import axios from "axios";
+import api from "../apis/api";
 import VerticalTable from "../components/VerticalTable";
+import ErrorModal from "../components/ErrorModal";
 import EditDeleteGroup from "../components/EditDeleteGroup";
 
 const headerAndKeys = [
-  {header: 'First Name:', key:'first_name'},
-  {header: 'Last Name:', key:'last_name'},
-  {header: 'Employee Id:', key: 'id'},
-  {header: 'Title:', key: 'title'},
-  {header: 'Department:', key: 'department'},
-  {header: 'Salary:', key: 'salary'},
-]
+  { header: "First Name:", key: "first_name" },
+  { header: "Last Name:", key: "last_name" },
+  { header: "Employee Id:", key: "id" },
+  { header: "Title:", key: "title" },
+  { header: "Department:", key: "department" },
+  { header: "Salary:", key: "salary" },
+];
 
 class EmployeePage extends React.Component {
-  state = { employee: null, employeeId: this.props.match.params.id };
+  state = {
+    employee: null,
+    employeeId: this.props.match.params.id,
+    errorMessage: "",
+  };
 
-  getEmployee = async () => {
-    const { data } = await axios.get(
-      `http://localhost:3001/employee/${this.state.employeeId}`
-    );
-    this.setState({ employee: data[0] });
+  getEmployee = () => {
+    api
+      .get(`/employee/${this.state.employeeId}`)
+      .then(({ data }) => {
+        this.setState({ employee: data[0] });
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          this.setState({ errorMessage: error.response.statusText });
+        } else if (error.request) {
+          console.log(error.request);
+          this.setState({
+            errorMessage:
+              "There was an error connecting to the server. Please reload the page.",
+          });
+        } else {
+          console.log("Error", error.message);
+          this.setState({
+            errorMessage: "There was an error loading the page.",
+          });
+        }
+        console.log(error.config);
+      });
   };
 
   handleDeleteClick = () => {
-    axios
-      .delete(`http://localhost:3001/employee/${this.state.employeeId}`)
-      .then(
-        (response) => {
-          if (response.status === 200) {
-            this.props.history.push("/employees");
-          }
-        },
-        (error) => {
-          console.log(error);
+    api.delete(`/employee/${this.state.employeeId}`).then(
+      (response) => {
+        if (response.status === 200) {
+          this.props.history.push("/roles");
         }
-      );
+      },
+      (error) => {
+        console.log(error);
+        this.setState({
+          errorMessage: "There was an error deleteing the role.",
+        });
+      }
+    );
   };
 
   componentDidMount() {
@@ -45,14 +70,23 @@ class EmployeePage extends React.Component {
     return (
       <div className="container mt-5 text-center">
         <h2>Employee Profile</h2>
-        <VerticalTable headersAndKeys={headerAndKeys} tableData={this.state.employee} />
-        <EditDeleteGroup
-          modalMessage="Are you sure you want to delete this employee?"
-          handleDeleteClick={this.handleDeleteClick}
-          linkTo={`${this.state.employeeId}/edit`}
-         /> 
+        {this.state.errorMessage ? (
+          <ErrorModal modalMessage={this.state.errorMessage} />
+        ) : (
+          <div>
+            <VerticalTable
+              headersAndKeys={headerAndKeys}
+              tableData={this.state.employee}
+            />
+            <EditDeleteGroup
+              modalMessage="Are you sure you want to delete this employee?"
+              handleDeleteClick={this.handleDeleteClick}
+              linkTo={`${this.state.employeeId}/edit`}
+            />
+          </div>
+        )}
       </div>
-    )
+    );
   }
 }
 
